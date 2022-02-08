0f0493da9918762e5597d99d6166f5d057cd74a8
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2022-02-08 12:46:03 -0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`
   },
+  pathPrefix: "/testing-sanity",
   plugins: [{
     resolve: 'gatsby-source-sanity',
     options: {
